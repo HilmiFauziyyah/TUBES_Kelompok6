b4f4de459ac00deb9acb584e5a4c44109e36b00a
Marsyarayyy <marsyarh09@gmail.com>
2023-05-14 14:51:20 +0700

    main awal

---
+++ AplikasiHotel/AplikasiHotel/Hotel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplikasiHotel
+{
+        public class Hotel
+        {
+
+            int jumlahKamar = 10;
+            int[] nomorKamar = new int[10] { 101, 102, 103, 104, 105, 106, 107, 108, 109, 110 };
+            string[] statusKamar = new string[10] { "tersedia", "tersedia", "tersedia", "tersedia", "tersedia", "tersedia", "tersedia", "tersedia", "tersedia", "tersedia" };
+            string[] namaPengunjung = new string[10];
+            int[] hargaKamar = new int[10] { 750000, 750000, 750000, 750000, 750000, 1000000, 1000000, 1000000, 1000000, 1500000 };
+
+            public void PesanKamar()
+            {
+                // Kontrak pra-kondisi
+                if (jumlahKamar < 1 || nomorKamar.Length != jumlahKamar || statusKamar.Length != jumlahKamar || namaPengunjung.Length != jumlahKamar)
+                {
+                    throw new ArgumentException("Parameter tidak valid.");
+                }
+
+
+                Console.WriteLine("Masukkan nomor kamar yang ingin dipesan: ");
+                int nomor = int.Parse(Console.ReadLine()); ;
+                
+
+                // Looping untuk mencari nomor kamar yang sesuai
+                for (int i = 0; i < jumlahKamar; i++)
+                {
+                    // Kamar ditemukan
+                    if (nomorKamar[i] == nomor && statusKamar[i] == "tersedia")
+                    {
+                        Console.Write("Masukkan nama pengunjung :");
+                        string nama = Console.ReadLine();
+                        namaPengunjung[i] = nama;
+                        statusKamar[i] = "terisi";
+
+                        // Kontrak pasca-kondisi
+                        if (namaPengunjung[i] != nama || statusKamar[i] != "terisi")
+                        {
+                            throw new InvalidOperationException("Operasi tidak berhasil dilakukan.");
+                        }
+
+                        Console.WriteLine("\nKamar " + nomor + " berhasil dipesan atas nama " + nama + ", dengan harga " + hargaKamar[i] + " per malam");
+                        return;
+                    }
+                }
+
+                Console.WriteLine("\nMaaf, kamar dengan nomor " + nomor + " tidak tersedia atau sudah terisi");
+
+                // Kontrak pasca-kondisi
+                for (int i = 0; i < jumlahKamar; i++)
+                {
+                    if (nomorKamar[i] == nomor && statusKamar[i] == "tersedia")
+                    {
+                        throw new InvalidOperationException("Operasi tidak berhasil dilakukan.");
+                    }
+                }
+            }
+        }
+
+}
--- AplikasiHotel/AplikasiHotel/Program.cs
+++ AplikasiHotel/AplikasiHotel/Program.cs
@@ -1,1 +1,38 @@
 ﻿
+using AplikasiHotel;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+
+        Hotel hotel = new Hotel();
+
+            while (true)
+            {
+                Console.WriteLine("\nMenu:");
+                Console.WriteLine("1. Cek Ketersediaan Kamar");
+                Console.WriteLine("2. Pesan Kamar");
+                Console.WriteLine("3. Cek Status Kamar");
+                Console.WriteLine("4. Checkout");
+                Console.WriteLine("5. Cek Harga");
+                Console.WriteLine("6. Pesan Makanan");
+                Console.WriteLine("7. Keluar dari Aplikasi");
+
+
+                Console.Write("\nPilihan Anda (1-6): ");
+                int pilihan = int.Parse(Console.ReadLine());
+
+                switch (pilihan)
+                {
+                    case 2:
+                        hotel.PesanKamar();
+                        break;
+
+                }
+            }
+        
+    }
+}
+
+
